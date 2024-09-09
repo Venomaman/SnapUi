8a49799c60477d3c519eed8c8f4330bf65e878c5
Venomaman <amankumaranand111@gmail.com>
2024-09-09 18:53:50 +0530

    initial commit started adding details in landing page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,68 @@
 import Image from "next/image";
+import Link from "next/link";
+import { FaUikit } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="">
+      <Navbar />
+      <AboutSection />
+    </div>
+  );
+}
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+function AboutSection() {
+  return (
+    <div className="flex flex-col mx-16 items-center mt-28 max-sm:mt-20 gap-6">
+      <h2 className="font-bold text-2xl max-sm:text-xl text-center text-slate-800">
+        Manage and Create Your React Components
+        <span className="text-sky-500"> Effortlessly!</span>
+      </h2>
+      <p className="text-center text-[15px] max-sm:text-[14px] w-[510px] max-sm:w-full text-slate-500">
+        Save time by reusing your favourit components. Store them 
+        in a centralized database and create new components with
+        ease. Enhance your development workflow by having quick access
+        to a library of reusable components and ensure consistency across
+        your projects.
+      </p>
+
+      <button className="bg-sky-600 hover:bg-sky-700 rounded-md text-sm font-medium px-9 py-3 text-white shadow-md" type="button">{`Let's get started!`}</button>
+    </div>
+  );
+}
+
+function Navbar() {
+  return (
+    <nav className="flex m-5 justify-between items-center max-sm:flex-col mx-8 max-sm:mt-9">
+      <Logo />
+      <Buttons />
+    </nav>
+  );
+}
+
+function Logo() {
+  return (
+    <div className="flex justify-center items-center gap-1 text-2xl max-sm:xl">
+      <p className="font-mono text-slate-500 font-semibold">SnapUi</p>
+      <FaUikit />
+    </div>
+  );
+}
+
+function Buttons() {
+  return (
+    <div className="flex gap-2 max-sm:flex-col max-sm:w-full max-sm:mt-8">
+      <Link href="/sign-up">
+        <button className="max-sm:w-full p-3 bg-sky-600 hover:text-white shadow-md rounded-xl px-6 sm:w-full">
+          Sign-up
+        </button>
+      </Link>
+
+      <Link href="/sign-in">
+        <button className="max-sm:w-full p-3 hover:bg-sky-600 hover:text-white border border-sky-500 shadow-md rounded-xl px-6 sm:w-full">
+          Sign-In
+        </button>
+      </Link>
     </div>
   );
 }
